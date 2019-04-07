ab38a9f2eba2d17f6729fbb61192ae614f612c24
Yassine KLILICH <klilichyassine8@gmail.com>
2019-04-07 20:32:26 +0100

    Progress Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+   loading: Boolean;
   
+   constructor(private router:Router){
+      this.loading = true;
+   }
+
+   ngAfterViewInit(){
+      this.router.events.subscribe((event)=>{
+      if(event instanceof NavigationStart)
+         this.loading = true;
+      if(event instanceof NavigationEnd)
+         this.loading = false;
+      
+      })
+   }
 }
