21c5cf552c8cff5323ab29077b201db424b4e5c1
Yassine KLILICH <klilichyassine8@gmail.com>
2019-04-08 12:47:09 +0200

    Adding custom scrollbar
    I learned how to style scrollbar and get referenced element from the template in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd, NavigationCancel } from "@angular/router";
 
 @Component({
@@ -6,20 +6,27 @@ import { Router, NavigationStart, NavigationEnd, NavigationCancel } from "@angul
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-   loading: Boolean;
-  
-   constructor(private router:Router){
-      this.loading = true;
-   }
+export class AppComponent implements OnInit, AfterViewInit {
+   
+   // @ViewChild('sidenav', {read: ElementRef}) sidenav: ElementRef;
+   // @ViewChild('listnav') listnav: ElementRef;
+   
+   loading : Boolean;
+   // sidenavWidth : String;
+   // listnavWidth : String;
 
+   constructor(private router:Router) { this.loading = true; }
+
+   ngOnInit(){
+      // this.sidenavWidth = window.getComputedStyle(this.sidenav.nativeElement).height;
+      // this.listnavWidth = window.getComputedStyle(this.listnav.nativeElement).height;
+   }
+   
    ngAfterViewInit(){
       this.router.events.subscribe((event)=>{
-      if(event instanceof NavigationStart)
-         this.loading = true;
-      if(event instanceof NavigationEnd)
-         this.loading = false;
-      
+         if(event instanceof NavigationStart) { this.loading = true; };
+         if(event instanceof NavigationEnd || event instanceof NavigationCancel) { setInterval(()=>{ this.loading = false; }, 2000); }
       })
    }
+
 }
